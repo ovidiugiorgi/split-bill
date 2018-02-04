66308f7a913a0ec56c81d736bab6dcce9ce60920
Ovidiu Giorgi <ovidiugiorgi@gmail.com>
2018-02-04 14:30:19 +0200

    Add pre save hook to encrypt passwords using bcrypt

--- be/app.js
+++ be/app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 const bluebird = require('bluebird');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const winston = require('winston');
 
 const index = require('./routes/index');
 const api = require('./routes/api.route');
@@ -65,5 +66,28 @@ mongoose.connect(mongoURL)
   .catch(() => {
     console.log(`Error Connecting to the Mongodb Database at URL: ${mongoURL}`)
   });
+
+const winstonLogger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    //
+    // - Write to all logs with level `info` and below to `combined.log`
+    // - Write all logs error (and below) to `error.log`.
+    //
+    new winston.transports.File({filename: 'error.log', level: 'error'}),
+    new winston.transports.File({filename: 'combined.log'})
+  ]
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+  winstonLogger.add(new winston.transports.Console({
+    format: winston.format.simple()
+  }));
+}
 
 module.exports = app;
--- be/models/user.model.js
+++ be/models/user.model.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const mongoosePaginate = require('mongoose-paginate');
 
+const bcrypt = require('bcrypt');
+
 const UserSchema = new mongoose.Schema({
   username: {
     type: String,
@@ -14,7 +16,19 @@ const UserSchema = new mongoose.Schema({
   }
 });
 
+UserSchema.pre('save', function (next) {
+  const user = this;
+  bcrypt.hash(user.password, 10, function (err, hash) {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  })
+});
+
 UserSchema.plugin(mongoosePaginate);
+
 const User = mongoose.model('User', UserSchema);
 
 module.exports = User;
